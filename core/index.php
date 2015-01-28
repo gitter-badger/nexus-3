a6a85b36398e422a93ae0e6e8669a965f0907383
Michael <mvd1409@gmail.com>
2015-01-27 21:43:03 -0500

    Create index.php

---
+++ core/index.php
@@ -0,0 +1,13 @@
+<!--Include Header Info-->
+<?php include 'header.php'; ?>
+<!---------------------------------->
+
+
+
+Homepage Web Content Goes Here
+
+
+
+<!---------------------------------->
+<!--Include Footer Info-->
+<?php include 'footer.php'; ?>
